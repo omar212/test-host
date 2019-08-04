18aa23df39e46922661fe8b1d1ad66a6db2df486
Omar Elnagdy <Omar.Elnagdy@ibm.com@Omars-MacBook-Pro.local>
2019-08-03 20:56:53 -0400

    responsive footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,48 +1,27 @@
 import React, { Component } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import './Footer.scss'
 
-const useStyles = makeStyles(theme => ({
-  title: {
-    flexGrow: 1,
-     margin: '0 auto',
-    fontWeight: 'none',
-    fontFamily: 'Times New Roman, Times, serif'
-  },
+const Footer = () => (
+    <div className="footer-wrapper">
+        <ul className="footer-contatiner">
+            <div className="logo-footer">
+              <li>
+                  <a href="/">Logo</a>
+              </li>
+            </div>
 
-  instagram: {
-    float: 'right'
-  }
-}));
+            <div className="cusr-footer">
+                  
+                  <a href="/">Contact Us</a>
+              
+                  <span className="menu__spacer">|</span>
 
+                  <a href="/">Shipping & Returns</a>
+              
+            </div>
+        </ul>
+    </div>
+)
 
-// const matches = useMediaQuery('(min-width:600px)');
-
-
-export default function ButtonAppBar()  {
-  const classes = useStyles();
-  // const matches = useMediaQuery();
-
-  return (  
-    <div>
-        <AppBar className="footer">
-          <Toolbar className="tool-text">
-            <Typography variant="h7" className={classes.title}>
-              . سف أخفر .  سف أخفر. سف أخفر. سف أخفر. سف أخفر
-            </Typography> <br />
-            {/* <Typography variant="h6" className={classes.instagram}>
-              Instagram: @sefgreenco
-            </Typography> */}
-          </Toolbar>
-        </AppBar>
-      </div> );
-
-}
-
-
-
+export default Footer
